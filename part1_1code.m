d27dc8fc0bdb74dc44294658556cf90a5b8049a9
Wesley <wesleyfinck@gmail.com>
2020-04-05 23:03:07 -0300

    changed so units were right

--- part1_1code.m
+++ part1_1code.m
@@ -1,12 +1,20 @@
 %% run before simulink simulation
 
-time = [0:0.001:3.5]';
-s1 = zeros(1,800)';
-s2 = 12.6*ones(1,314)';
-s3 = zeros(1,800)';
-s4 = 12.4*ones(1,314)';
-s5 = zeros(1,800)';
-s6 = zeros(1,473)';
+% time = [0:0.001:3.5]';
+% s1 = zeros(1,800)';
+% s2 = 12.6*ones(1,314)';
+% s3 = zeros(1,800)';
+% s4 = 12.4*ones(1,314)';
+% s5 = zeros(1,800)';
+% s6 = zeros(1,473)';
+
+time = [0:0.01:15]';
+s1 = zeros(1,400)';
+s2 = 12.624*ones(1,31)';
+s3 = zeros(1,400)';
+s4 = 13.08*ones(1,31)';
+s5 = zeros(1,400)';
+s6 = zeros(1,239)';
 
 data = [s1;-1*s2;s3;s4;s5;s6];
 data1 = cat(1,s1,-1*s2);
@@ -15,7 +23,7 @@ data3 = cat(1,data2,s2);
 data4 = cat(1,data3,s1);
 %data = cat(1,data4,s6);
 
-rdata = cat(1, 25*ones(3028,1), zeros(473,1));
+rdata = cat(1, 2.5*ones(1262,1), zeros(239,1));
 
 r = [time,rdata];
 
